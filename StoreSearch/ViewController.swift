db81ef65bf74411b75ab691ecf358de71f049cc3
Tyler Bischoff <tbisch1@live.spcollege.edu>
2022-02-16 00:24:38 -0500

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Tyler Bischoff on 2/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
