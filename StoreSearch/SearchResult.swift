49533bfb76e2e30d04ae1f24c9734fdd9ae334a5
Tyler Bischoff <tbisch1@live.spcollege.edu>
2022-02-16 02:23:53 -0500

    end of ch 32

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Tyler Bischoff on 2/16/22.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
